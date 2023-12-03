3c3a6ea91a4cbf1154e730b0b415b461931d8c51
akotonystark <alarbiampofo@gmail.com>
2023-12-03 08:42:52 -0500

    updated portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,10 +12,10 @@ function Portfolio() {
 			<div className="filters">
 				<ul>
 					<li className="active" data-filter="*">All</li>
-					<li data-filter=".packaging">Packaging</li>
-					<li data-filter=".mockup">Mockup</li>
-					<li data-filter=".typography">Typography</li>
-					<li data-filter=".photography">Photography</li>
+					<li data-filter=".packaging">Client Projects</li>
+					<li data-filter=".mockup">Personal Projects </li>
+					<li data-filter=".typography">Mockups</li>
+					{/* <li data-filter=".photography">Photography</li> */}
 				</ul>
 			</div>
 
@@ -27,102 +27,123 @@ function Portfolio() {
 								<div className="overlay overlay-bg"></div>
 								<img className="image img-fluid" src="images/p1.jpg" alt=""/>
 							</div>
-							<a href="images/p1.jpg" className="img-pop-up">  
+							<a href="https://space-trips.netlify.app/" className="img-pop-up">  
 								<div className="middle">
 									<div className="text align-self-center d-flex"><img src="images/preview.png" alt=""/></div>
 								</div>
 							</a>                                  
 						</div>
-						<div className="p-inner">
+						<a href="https://space-trips.netlify.app/"><div className="p-inner">
 							<h4>Space Trips</h4>
 							<div className="cat">Mapbox Application</div>
-						</div>                                         
+						</div></a>                                         
 					</div>
+
 					<div className="single-portfolio col-sm-4 all mockup">
 						<div className="relative">
 							<div className="thumb">
 								<div className="overlay overlay-bg"></div>
-								<img className="image img-fluid" src="images/p2.jpg" alt=""/>
+								<img className="image img-fluid" src="images/p2.png" alt=""/>
 							</div>
-							<a href="images/p2.jpg" className="img-pop-up">  
+							<a href="images/p2.png" className="img-pop-up">  
 								<div className="middle">
 									<div className="text align-self-center d-flex"><img src="images/preview.png" alt=""/></div>
 								</div>
 							</a>                                  
 						</div>
-						<div className="p-inner">
-							<h4>Todo App</h4>
-							<div className="cat">Drag n Drop</div>
-						</div>                                         
-					</div>                            
+						<a href='https://todo-app-webfx.netlify.app/'>
+							<div className="p-inner">
+								<h4>Todo App</h4>
+								<div className="cat">Drag n Drop</div>
+							</div>       
+						</a>                                  
+					</div>    
+
 					<div className="single-portfolio col-sm-4 all packaging">
 						<div className="relative">
 							<div className="thumb">
 								<div className="overlay overlay-bg"></div>
-								<img className="image img-fluid" src="images/p3.jpg" alt=""/>
+								<img className="image img-fluid" src="images/post_2.jpg" alt=""/>
 							</div>
-							<a href="images/p3.jpg" className="img-pop-up">  
+							<a href="images/erp.png" className="img-pop-up">  
 								<div className="middle">
 									<div className="text align-self-center d-flex"><img src="images/preview.png" alt=""/></div>
 								</div>
 							</a> 
 
 						</div>
+						<a href="images/erp.png">
 						<div className="p-inner">
 							<h4>Biz 360</h4>
 							<div className="cat">ERP Application</div>
 						</div>
+						</a>
+						
 					</div>
+
 					<div className="single-portfolio col-sm-4 all packaging">
 						<div className="relative">
 							<div className="thumb">
 								<div className="overlay overlay-bg"></div>
-								<img className="image img-fluid" src="images/p4.jpg" alt=""/>
+								<img className="image img-fluid" src="images/pos.jpeg" alt=""/>
 							</div>
-							<a href="images/p4.jpg" className="img-pop-up">  
+							<a href="images/tinatett.png" className="img-pop-up">  
 								<div className="middle">
 									<div className="text align-self-center d-flex"><img src="images/preview.png" alt=""/></div>
 								</div>
 							</a>                                
 						</div> 
+						<a href="images/tinatett.png">
 						<div className="p-inner">
 							<h4>Tinatett</h4>
 							<div className="cat">POS Application</div>
 						</div>
+						</a>
+						
 					</div>
-					<div className="single-portfolio col-sm-4 all typography">
+
+					<div className="single-portfolio col-sm-4 all packaging">
 						<div className="relative">
 							<div className="thumb">
 								<div className="overlay overlay-bg"></div>
 								<img className="image img-fluid" src="images/p5.jpg" alt=""/>
 							</div>
-							<a href="images/p5.jpg" className="img-pop-up">  
+							<a href="images/wp.png" className="img-pop-up">  
 								<div className="middle">
 									<div className="text align-self-center d-flex"><img src="images/preview.png" alt=""/></div>
 								</div>
 							</a>                                
 						</div>
+
+						<a href="images/wp.png">
 						<div className="p-inner">
 							<h4>Wedding Planner</h4>
 							<div className="cat">Event Planning App</div>
 						</div>
+						</a>
+						
 					</div>
-					<div className="single-portfolio col-sm-4 all photography">
+					
+					<div className="single-portfolio col-sm-4 all typography">
 						<div className="relative">
 							<div className="thumb">
 								<div className="overlay overlay-bg"></div>
-								<img className="image img-fluid" src="images/p6.jpg" alt=""/>
+								<img className="image img-fluid" src="images/fig.jpg" alt=""/>
 							</div>
-							<a href="images/p6.jpg" className="img-pop-up">  
+							<a href="https://www.figma.com/file/57YzqUDPNcWJQcDhGwk8yd/ProjectX?type=design&node-id=0%3A1&mode=design&t=X7EfGNT0wlRLOKV7-1" className="img-pop-up">  
 								<div className="middle">
 									<div className="text align-self-center d-flex"><img src="images/preview.png" alt=""/></div>
 								</div>
 							</a>                                
 						</div>
-						<div className="p-inner">
-							<h4>Rental App (React Native)</h4>
-							<div className="cat">Mockup</div>
-						</div>
+
+						<a href="https://www.figma.com/file/57YzqUDPNcWJQcDhGwk8yd/ProjectX?type=design&node-id=0%3A1&mode=design&t=X7EfGNT0wlRLOKV7-1">
+							<div className="p-inner">
+								<h4>Rental App (React Native)</h4>
+								<div className="cat">Mockup</div>
+							</div>
+						</a>
+						
 					</div>
 				</div>
 			</div>
